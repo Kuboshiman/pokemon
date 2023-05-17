12c319a46953c807f0349b9f370828051fd3d78b
Kuboshiman <kubo.shogo@arakawa-lab.com>
2023-05-17 16:51:07 +0900

    poke_list_itemファイルの作成と整形

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'poke_list_item.dart';
 import 'poke_detail.dart';
 
 void main() {
@@ -46,41 +47,6 @@ class PikaButton extends StatelessWidget {
             builder: (BuildContext context) => const PokeDetail(),
           ),
         ),
-      },
-    );
-  }
-}
-
-class PokeListItem extends StatelessWidget {
-  const PokeListItem({Key? key, required this.index}) : super(key: key);
-  final int index;
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      leading: Container(
-        width: 80,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          image: const DecorationImage(
-            fit: BoxFit.fitWidth,
-            image: NetworkImage(
-                "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png"),
-          ),
-        ),
-      ),
-      title: const Text(
-        'pikachu',
-        style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-      ),
-      subtitle: const Text(
-        '⚡︎electric',
-      ),
-      trailing: const Icon(Icons.navigate_next),
-      onTap: () => {
-        Navigator.of(context).push(MaterialPageRoute(
-          builder: (BuildContext context) => const PokeDetail(),
-        ))
       },
     );
   }
