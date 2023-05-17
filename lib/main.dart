3e99d5c0adde1021c72063e7018a7982d4df3745
Kuboshiman <kubo.shogo@arakawa-lab.com>
2023-05-17 16:34:40 +0900

    ListTileでリスト表示

--- lib/main.dart
+++ lib/main.dart
@@ -23,16 +23,18 @@ class TopPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: PikaButton(),
+    return Scaffold(
+      body: ListView.builder(
+        itemCount: 100,
+        itemBuilder: (context, index) => PokeListItem(index: index),
       ),
     );
   }
 }
 
 class PikaButton extends StatelessWidget {
-  const PikaButton({Key? key}) : super(key: key);
+  const PikaButton({Key? key, required this.index}) : super(key: key);
+  final int index;
 
   @override
   Widget build(BuildContext context) {
@@ -44,6 +46,41 @@ class PikaButton extends StatelessWidget {
             builder: (BuildContext context) => const PokeDetail(),
           ),
         ),
+      },
+    );
+  }
+}
+
+class PokeListItem extends StatelessWidget {
+  const PokeListItem({Key? key, required this.index}) : super(key: key);
+  final int index;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Container(
+        width: 80,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          image: const DecorationImage(
+            fit: BoxFit.fitWidth,
+            image: NetworkImage(
+                "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png"),
+          ),
+        ),
+      ),
+      title: const Text(
+        'pikachu',
+        style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+      ),
+      subtitle: const Text(
+        '⚡︎electric',
+      ),
+      trailing: const Icon(Icons.navigate_next),
+      onTap: () => {
+        Navigator.of(context).push(MaterialPageRoute(
+          builder: (BuildContext context) => const PokeDetail(),
+        ))
       },
     );
   }
